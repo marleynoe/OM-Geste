095b869d2d9e3fd699851eea9478a88120183cab
Marlon Schumacher <m_schumacher@users.sourceforge.net>
2015-04-19 22:46:48 -0400

    started making filters recursive

--- sources/gesture-filters.lisp
+++ sources/gesture-filters.lisp
@@ -1,37 +1,101 @@
 (in-package :om)
 
 ; some filtering functions
+; I should make these filters recursive!
 
 ; Max's "slide filter"
 (defmethod! slide-filter ((self list) (slide number))
-            (print self)
-            (let ((lastsample (car self)))
-              (print self)
+            (let ((lastsample (car self)))             
               (loop for sample in self collect
                     (setf lastsample (+ lastsample (/ (- sample lastsample) slide)))
                     )
               ))
 
+(defmethod! slide-filter ((self list) (slide number) &optional recursion)
+            :icon 631  
+            :initvals '(nil nil 1)
+            :indoc '("a list, bpf, bpc, 3dc, 3d-trajectory or libs thereof" "a number" "a number")
+            :numouts 1
+            :doc "Implements the filter function y (n) = y (n-1) + ((x (n) - y (n-1))/slide"
+            (let ((lastsample (car self))
+                  (thelist self))
+              (loop for i from 1 to recursion do
+                    (setf thelist (print (loop for sample in thelist collect
+                                        (setf lastsample (+ lastsample (/ (- sample lastsample) slide)))
+                                        ))
+                          ))
+                    thelist))
 
-; simple-moving-average
-(defmethod! sma ((self list) (windowsize number))
-            (let ((windowedlist (print (x-append (repeat-n (car self) windowsize) self))))    
-           ;  (let ((windowedlist self)) 
-              (loop for window in self collect
-                      (om-mean (first-n (setf windowedlist (cdr windowedlist)) windowsize))
-                        )
-                  ))
+(defmethod! slide-filter ((self bpf) (windowsize number))
+            (let ((xpoints (x-points self))
+                  (ypoints (slide-filter (y-points self) windowsize)))
+              (simple-bpf-from-list xpoints ypoints 'bpf (decimals self))
+              ))
+
+;probably not the proper way of doing this (with dimensionality >1)
+(defmethod! slide-filter ((self bpc) (windowsize number))
+            (let ((xpoints (slide-filter (x-points self) windowsize))
+                  (ypoints (slide-filter (y-points self) windowsize)))
+              (simple-bpf-from-list xpoints ypoints 'bpc (decimals self))
+              ))
+
+(defmethod! slide-filter ((self 3DC) (windowsize number))
+            (let ((xpoints (slide-filter (x-points self) windowsize))
+                  (ypoints (slide-filter (y-points self) windowsize))
+                  (zpoints (slide-filter (z-points self) windowsize)))
+              (3DC-from-list xpoints ypoints zpoints '3dc (decimals self))
+              ))
+
+; simple-moving-average 
+(defmethod! sma ((self list) (windowsize number) &optional recursion)
+            :icon 631  
+            :initvals '(nil nil 1)
+            :indoc '("a list, bpf, bpc, 3dc, 3d-trajectory or libs thereof" "a number" "a number")
+            :numouts 1
+            :doc "Implements the simple-moving-average: the average of a list of numbers in a sliding winwod."
+            (let ((windowedlist (x-append (repeat-n (car self) windowsize) self))
+                  (thelist self))
+              (loop for i from 1 to recursion do
+                    (setf thelist (loop for window in thelist collect
+                                        (om-mean (first-n (setf windowedlist (cdr windowedlist)) windowsize)))
+                          ))
+              thelist))
+
+(defmethod! sma ((self bpf) (windowsize number))
+            (let ((xpoints (x-points self))
+                  (ypoints (sma (y-points self) windowsize)))
+              (simple-bpf-from-list xpoints ypoints 'bpf (decimals self))
+              ))
+
+(defmethod! sma ((self 3dc) (windowsize number))
+            (let ((xpoints (sma (x-points self) windowsize))
+                  (ypoints (sma (y-points self) windowsize))
+                  (zpoints (sma (z-points self) windowsize)))
+              (3dc-from-list xpoints ypoints zpoints '3dc (decimals self))
+              ))
 
 ; simple-moving-median
 (defmethod! smm ((self list) (windowsize number))
-            (let ((windowedlist (print (x-append (repeat-n (car self) windowsize) self))))    
+            (let ((windowedlist (x-append (repeat-n (car self) windowsize) self)))    
            ;  (let ((windowedlist self)) 
               (loop for window in self collect
-                    (nth (print (ceiling (* 0.5 windowsize)))
+                    (nth (ceiling (* 0.5 windowsize))
                       (sort-list (first-n (setf windowedlist (cdr windowedlist)) windowsize))
                         ))
                   ))
 
+(defmethod! smm ((self bpf) (windowsize number))
+            (let ((xpoints (x-points self))
+                  (ypoints (smm (y-points self) windowsize)))
+              (simple-bpf-from-list xpoints ypoints 'bpf (decimals self))
+              ))
+
+(defmethod! smm ((self 3dc) (windowsize number))
+            (let ((xpoints (smm (x-points self) windowsize))
+                  (ypoints (smm (y-points self) windowsize))
+                  (zpoints (smm (z-points self) windowsize)))
+              (3dc-from-list xpoints ypoints zpoints '3dc (decimals self))
+              ))
 
 ; write recursively?
                 
--- sources/mapping.lisp
+++ sources/mapping.lisp
@@ -24,6 +24,7 @@
 
 ;;make methods for lists of processes!
 
+#|
 (defmethod! mapping ((self gesture-model) matching-fun theclass)
             :icon '(333)
             (compile-patch matching-fun)
@@ -52,10 +53,12 @@
                           )
                     box
                     )))
+|#
 
-
-
-(defmethod! mapping2 ((self gesture-model) matching-fun)
+; To do: 1) needs to work with other classes (not only temporalboxes), 2) need a map-column, map-row? function (where the lambda patch can see the entire picture)
+; if I want to have dynamic mappins I need to be able to see the entire model.
+ 
+(defmethod! mapping ((self gesture-model) matching-fun)
             :icon '(333)
             ;(print "it's me")
             (compile-patch matching-fun)
@@ -81,6 +84,47 @@
                          (slots (mat-trans (list names vals))))
                     
                     (setf (free-store box) vals)
+                    
+                    (loop for item in slots do
+                          (if (is-om-slot? (type-of box) (car item))
+                              
+                              (set-slot box  (car item) (if (floatp (cadr item))
+                                                            (om-round (cadr item))
+                                                          (cadr item)))
+                            (om-beep-msg (format nil "Error: slot ~A does not exist in class TemporalBox !" (car item))))
+                          )
+                    box
+                    )))
+
+
+; think about arrays: maybe in that case I should rather set components than making different instances.
+; also, the slots of the instances actually DO get set, but it's not visible in the editor. -> Jean??
+(defmethod! mapping2 ((self gesture-model) matching-fun theclass)
+            :icon '(333)
+            ;(print "it's me")
+            (compile-patch matching-fun)
+            (loop for col from 0 to (1- (numcols self)) collect
+                  (let* ((box (make-instance (type-of theclass)))
+                         (thecontrols (lcontrols self))
+                         (vals 
+                          (multiple-value-list 
+                           (funcall (intern (string (code matching-fun)) :om) ;matching fun = patch in lambda mode
+
+                                   ; (loop for slot in (get-all-initargs-of-class (type-of self)) collect
+                                   ;       (print (list (name slot)
+                                   ;             (get-array-val self (name slot) col)))))))
+                                   ; a list of ( (("name1" val@col1) ("name2" val@col1)) (("name1" val@col2) ("name2" val@col2)))                                   
+                                   ; (lcontrols self))))
+
+                                    (loop for slot in thecontrols collect
+                                          (list (string (first slot)) (nth col (second slot)))))))
+                                                 ;slot))))
+                         (names (mapcar #'(lambda (out) 
+                                            (intern (frame-name out) :om))
+                                        (sort (find-class-boxes (boxes matching-fun) 'omout) '< :key 'indice)))
+                         (slots (print (mat-trans (list names vals)))))
+                    
+                    ;(setf (free-store box) vals)
                     
                     (loop for item in slots do
                           (if (is-om-slot? (type-of box) (car item))
